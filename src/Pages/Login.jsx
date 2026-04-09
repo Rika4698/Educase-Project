57642a28c6ce3f9c9acc4a80f934810b60e34768
Rika4698 <rekasharmin46@gmail.com>
2026-04-09 12:00:00 +0600

    include login page

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    const navigate = useNavigate();
+    const [isValid , setIsValid] = useState(false);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Login;
